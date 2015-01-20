a4e45bf32300605165726627a72e0bfcd0203257
Darren Armstrong <darren.armstrong85@gmail.com>
2015-01-20 17:20:21 +0000

    Dummy Scala Swing Window.

--- src/ScalaQuery.scala
+++ src/ScalaQuery.scala
@@ -1,3 +1,16 @@
+import scala.swing._
+
+class HelloWorld extends SimpleSwingApplication {
+  def top = new MainFrame {
+    title = "Hello World GUI"
+    contents = new Button {
+      text = "Click me"
+    }
+  }
+}
+
 object ScalaQuery extends App {
   Console.println("Hello World: " + (args mkString ", "))
+  val h = new HelloWorld()
+  h.startup(args)
 }
